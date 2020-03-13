842d5cbe102ab69bbee9a28f98c369f62fef7900
Dmitry Kochetkov <dimitri.kochetkov@gmail.com>
2020-03-13 23:59:46 +0300

    Add a Spring module

---
+++ ege-search-engine/src/main/java/dimedrol/ege_search/EgeSearchEngineApplication.java
@@ -0,0 +1,13 @@
+package dimedrol.ege_search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgeSearchEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EgeSearchEngineApplication.class, args);
+    }
+
+}
---
+++ ege-search-engine/src/test/java/dimedrol/ege_search/EgeSearchEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package dimedrol.ege_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgeSearchEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
